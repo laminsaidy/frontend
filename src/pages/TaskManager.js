60fa84bbe7d5b1d9056220b62459f32d61280519
LX <132520325+laminsaidy@users.noreply.github.com>
2025-02-09 18:26:44 +0100

    Add UI rendering methods to TaskManager.js

--- src/pages/TaskManager.js
+++ src/pages/TaskManager.js
@@ -45,6 +45,58 @@ class TaskManager extends Component {
       .delete(`http://localhost:8000/api/tasks/${item.id}/`)
       .then((res) => this.refreshList());
   };
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(item => item.completed === viewCompleted);
+    return newItems.map(item => (
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+        <span className={`todo-title mr-2 ${viewCompleted ? "completed-todo" : ""}`} title={item.description}>
+          {item.title}
+        </span>
+        <span>
+          <button onClick={() => this.editItem(item)} className="btn btn-secondary mr-2">Edit</button>
+          <button onClick={() => this.handleDelete(item)} className="btn btn-danger">Delete</button>
+        </span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <button onClick={this.createItem} className="btn btn-primary">Add task</button>
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {this.state.modal && (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        )}
+      </main>
+    );
+  }
+  
+  
 }
 
 export default TaskManager;
