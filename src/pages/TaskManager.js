abb188da6cadb126724c18a1a0b9a956bd399b11
LX <132520325+laminsaidy@users.noreply.github.com>
2025-02-09 18:22:37 +0100

    Add modal toggle and CRUD functionality to TaskManager.js

--- src/pages/TaskManager.js
+++ src/pages/TaskManager.js
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
-import axios from "axios";  
+import axios from "axios";
 
 class TaskManager extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          viewCompleted: false,
-          activeItem: { title: "", description: "", completed: false },
-          taskList: [],
-          modal: false,
-        };
-    }
-      
-    componentDidMount() {
-        this.refreshList();
-    }
-      
-    refreshList = () => {
-        axios
-          .get("http://localhost:8000/api/tasks/")
-          .then(res => this.setState({ taskList: res.data }))
-          .catch(err => console.log(err));
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      activeItem: { title: "", description: "", completed: false },
+      taskList: [],
+      modal: false,
     };
+  }
 
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/tasks/")
+      .then((res) => this.setState({ taskList: res.data }))
+      .catch((err) => console.log(err));
+  };
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = (item) => {
+    this.toggle();
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    axios
+      .post("http://localhost:8000/api/tasks/", item)
+      .then((res) => this.refreshList());
+  };
+
+  handleDelete = (item) => {
+    axios
+      .delete(`http://localhost:8000/api/tasks/${item.id}/`)
+      .then((res) => this.refreshList());
+  };
 }
-export default TaskManager; 
+
+export default TaskManager;
