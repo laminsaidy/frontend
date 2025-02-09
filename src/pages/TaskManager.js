cfccd212ecebcac74b3a59a20c48c8697a327f5d
LX <132520325+laminsaidy@users.noreply.github.com>
2025-02-09 18:17:58 +0100

    Create state management and API fetching in TaskManager.js

--- src/pages/TaskManager.js
+++ src/pages/TaskManager.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import axios from "axios";  
 
 class TaskManager extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Task Manager</h1>
-      </div>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+          viewCompleted: false,
+          activeItem: { title: "", description: "", completed: false },
+          taskList: [],
+          modal: false,
+        };
+    }
+      
+    componentDidMount() {
+        this.refreshList();
+    }
+      
+    refreshList = () => {
+        axios
+          .get("http://localhost:8000/api/tasks/")
+          .then(res => this.setState({ taskList: res.data }))
+          .catch(err => console.log(err));
+    };
 
-export default TaskManager;
+}
+export default TaskManager; 
