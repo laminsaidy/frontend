449978470d9dc28f03636a77ff6f7428510dc8e5
LX <132520325+laminsaidy@users.noreply.github.com>
2025-02-09 17:56:28 +0100

    Created TaskManager component

---
+++ src/pages/TaskManager.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TaskManager extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Task Manager</h1>
+      </div>
+    );
+  }
+}
+
+export default TaskManager;
