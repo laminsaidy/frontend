f865f203bf51affbb27642e73e92bd05afc8ea3b
LX <132520325+laminsaidy@users.noreply.github.com>
2025-07-30 16:38:43 +0200

    Add lazy loading for static pages and wrap routes with Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
@@ -8,11 +8,13 @@ import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
 import TaskManager from "./pages/TaskManager";
 import TaskDetail from "./pages/TaskDetail";
-import TermsOfService from "./pages/TermsOfService";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import NotFound from "./pages/NotFound";
 import ScrollToTop from "./utils/ScrollToTop";
 import ErrorBoundary from "./components/ErrorBoundary";
+
+// Lazy-loaded components
+const TermsOfService = lazy(() => import("./pages/TermsOfService"));
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
@@ -22,35 +24,37 @@ function App() {
         <Navbar />
         <div className="app-content">
           <ErrorBoundary>
-            <Routes>
-              {/* Public routes */}
-              <Route path="/" element={<Homepage />} />
-              <Route path="/login" element={<Loginpage />} />
-              <Route path="/register" element={<Registerpage />} />
-              <Route path="/terms" element={<TermsOfService />} />
-              <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                {/* Public routes */}
+                <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Loginpage />} />
+                <Route path="/register" element={<Registerpage />} />
+                <Route path="/terms" element={<TermsOfService />} />
+                <Route path="/privacy" element={<PrivacyPolicy />} />
 
-              {/* Protected routes */}
-              <Route
-                path="/tasks"
-                element={
-                  <PrivateRoute>
-                    <TaskManager />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/task/:id"
-                element={
-                  <PrivateRoute>
-                    <TaskDetail />
-                  </PrivateRoute>
-                }
-              />
+                {/* Protected routes */}
+                <Route
+                  path="/tasks"
+                  element={
+                    <PrivateRoute>
+                      <TaskManager />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/task/:id"
+                  element={
+                    <PrivateRoute>
+                      <TaskDetail />
+                    </PrivateRoute>
+                  }
+                />
 
-              {/* Catch-all route */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+                {/* Catch-all route */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Suspense>
           </ErrorBoundary>
         </div>
       </AuthProvider>
