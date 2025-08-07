fc5ebedc179b1a866751d4d38aba5ddc0d6e873e
LX <132520325+laminsaidy@users.noreply.github.com>
2025-08-07 23:50:35 +0200

    add console.log for debugging

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,6 +5,8 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
+console.log("Base URL:", API_BASE_URL);
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
